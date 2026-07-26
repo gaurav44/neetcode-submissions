ebb50fa1ae5d4d32caf884973849353f36149554
Gaurav Gokhale <gaurav.gokhale44@gmail.com>
2026-07-26 17:41:51 +0200

    Add: minimum-interval-including-query - submission-4

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-4.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    void queryInterval(const vector<vector<int>>& intervals, int query, vector<int>& output) {
+        for(const auto& interval: intervals) {
+            if(query >= interval[0] && query <= interval[1]) {
+                output.push_back(interval[1] - interval[0] + 1);
+                return;
+            }   
+        }
+        output.push_back(-1);
+    };
+public:
+    vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
+            return  a[1] - a[0] + 1 < b[1] - b[0] + 1;
+        });
+
+        vector<int> output;
+
+        for(const auto query: queries) {
+            queryInterval(intervals, query, output);
+        }
+
+        return output;
+        
+    }
+};
