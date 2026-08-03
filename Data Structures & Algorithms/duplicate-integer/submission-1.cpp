cf29828cea883d49b7c611cfa12455192e573bd3
Gaurav Gokhale <gaurav.gokhale44@gmail.com>
2026-08-03 22:42:24 +0200

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> count;
+
+        for(const auto num : nums) {
+            if(count.contains(num)) return true;
+
+            count[num]++;
+        }
+        
+        return false;
+    }
+};
