e62bd55c2a56fc2ba66d46ca9491fef6378eac31
Gaurav Gokhale <gaurav.gokhale44@gmail.com>
2026-08-03 23:22:18 +0200

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        unordered_map<string, int> count;
+        vector<vector<string>> res;
+
+        for(const auto& str: strs) {
+            string tmp = str;
+            sort(tmp.begin(), tmp.end());
+            if(count.contains(tmp)) 
+                res.at(count[tmp]).push_back(str);
+            else {
+                res.push_back({str});
+                count[tmp] = res.size() - 1;
+            }
+        }
+        return res;        
+    }
+};
