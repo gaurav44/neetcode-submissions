581cf6eeb4cbb065d53a90b4334af694389e7b2c
Gaurav Gokhale <gaurav.gokhale44@gmail.com>
2026-08-09 17:40:51 +0200

    Add: pow-x-n - submission-6

---
+++ Data Structures & Algorithms/pow-x-n/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    double helper(double x, long n) {
+        if(n == 0) return 1;
+        
+        double half = helper(x, n / 2);
+
+        return (n % 2 == 0) ? half * half : x * half * half;
+    }
+public:
+    double myPow(double x, int n) {
+        if(x == 0) return 0;
+
+        if(n == 0) return 1;
+
+        double res = helper(x, abs(static_cast<long>(n)));
+
+        return n >= 0 ? res : 1 / res;
+    }
+};
