c75d314e4b356bf3a637a528716f1f184ae29b36
Gaurav Gokhale <gaurav.gokhale44@gmail.com>
2026-08-27 14:03:25 +0200

    Add: palindrome-partitioning - submission-9

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-9.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+
+        vector<vector<string>> res;
+        vector<string> curr;
+
+        auto isPalindrome = [&](auto&& self, string s) {
+            string temp = s;
+            reverse(temp.begin(), temp.end());
+            return s == temp;
+        };
+
+        auto dfs = [&](auto&& self, int start) {
+            if(start == s.size()) {
+                res.push_back(curr);
+                return;
+            } 
+
+            for(int end = start; end < s.size(); end++) {
+                string piece = s.substr(start, end - start + 1);
+
+                if(!isPalindrome(isPalindrome, piece)) continue;
+                
+                curr.push_back(piece);
+                self(self, end+1);
+                curr.pop_back();
+            }
+        };
+
+        dfs(dfs, 0);
+
+        return res;
+        
+    }
+};
